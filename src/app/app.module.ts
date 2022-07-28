e983a924f14ef861c932da25cfd4709f40ebb245
Simon.Viegas <simonviegas@outlook.com.br>
2022-07-28 20:07:57 -0300

    added PhotosModuler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
+import { PhotosModule } from "./photos/photos.module";
 
-import { PhotoComponent } from "./photo/photo.component";
+//import { PhotoComponent } from "./photos/photo/photo.component";
 
 @NgModule({
-  declarations: [AppComponent, PhotoComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
